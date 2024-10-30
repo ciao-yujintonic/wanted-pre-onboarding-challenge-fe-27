2aa4261d2a2209f6666eb50a87a0aa8ad647ad21
ciao-yujintonic <yujin.hong420@gmail.com>
2024-10-30 23:32:48 +0900

    ✏️ typo

--- src/main.tsx
+++ src/main.tsx
@@ -12,5 +12,5 @@ createRoot(document.getElementById('root')!).render(
     <ThemeProvider theme={theme}>
       <RouterProvider router={router} />
     </ThemeProvider>
-  </StrictMode>,
+  </StrictMode>
 )
